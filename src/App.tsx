2152e94fe6a1da380cc34b8004d1ef1cd4eb5bd7
Alba Casado <albaacc01@gmail.com>
2024-02-28 17:49:21 +0100

    mi app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Counter } from "./components/Counter"
+
+
+function App() {
+  
+
+  return (
+   <>
+      <h1>Bases React</h1>
+      <hr />
+
+      <Counter />
+
+   </>
+  )
+}
+
+export default App
---
+++ src/components/Counter.tsx
@@ -0,0 +1,8 @@
+
+export const Counter = () => {
+  return (
+    <div>
+        <h3>Counter: useState</h3>
+    </div>
+  )
+}
